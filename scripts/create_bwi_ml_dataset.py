624550901e1ae79fe5ba0fa32a5981ab69b56a09
hansendav <davhansen@web.de>
2024-09-24 20:43:45 +0200

    Adding exploration notebook + script draft

---
+++ scripts/create_bwi_ml_dataset.py
@@ -0,0 +1,5 @@
+# Python script to download and extract zip files from URLs listed in a config file.
+# Usage: python download_dbs.py --config_file config.yml
+# Config file should be in YAML format and contain the following
+# dest_folder: path to the folder where the zip files will be downloaded and extracted
+# urls: a dictionary of key-value pairs where key is the name of the extracted folder and value is the URL to download the zip file
